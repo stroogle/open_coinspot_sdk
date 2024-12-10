9e84fa2dfb891d773c0bfaa6a3d41f450533dd89
Colm Moore <colm.moore3@gmail.com>
2024-12-10 21:29:27 +1100

    🏷️ Created and implemented a CoinSpotResult type which will be used across development of the v2 sdk

--- src/v2/mod.rs
+++ src/v2/mod.rs
@@ -1,6 +1,32 @@
+
 //! Intereacts with the public/open section of the coinspot API.
 //! No authentication is required for this section of the library.
 pub mod public_api;
+
+use std::error::Error;
+use serde::{Deserialize, Serialize};
+
+/// CoinSpot's api documentation says that any bad response will fall into this format.
+/// ```
+/// CoinSpotBadResponse {
+///     staus: "error",
+///     message: "Coin not found"
+/// }
+/// ```
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CoinSpotBadResponse {
+    pub status: String,
+    pub message: String,
+}
+
+/// This enum encompasses the expected responses from the CoinSpot api. It uses a generic type enable easier pattern matching.
+pub enum CoinSpotResponse<T> {
+    Ok(T),
+    Bad(CoinSpotBadResponse)
+}
+
+/// This is an alias of Rust's Result type, the generic passed in will map to the Ok type in CoinSpotResponse.
+pub type CoinSpotResult<T> = Result<CoinSpotResponse<T>, Box<dyn Error>>;
 
 /// The main entry point for the public API methods.
 /// All methods are static.
--- src/v2/public_api/latest_prices.rs
+++ src/v2/public_api/latest_prices.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
-use std::error::Error;
-use crate::v2::CoinSpotPublic;
+use crate::v2::{CoinSpotBadResponse, CoinSpotPublic, CoinSpotResponse, CoinSpotResult};
+use reqwest::StatusCode;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -23,12 +23,24 @@ impl CoinSpotPublic {
     /// <https://www.coinspot.com.au/v2/api#latestprices>
     /// 
     /// Worth noting that even though the docs specify message: "ok" to exist on a successful request, it doesn't exist.
-    pub async fn latest_prices() -> Result<LatestPricesResponse, Box<dyn Error>> {
+    pub async fn latest_prices() -> CoinSpotResult<LatestPricesResponse> {
         let res = reqwest::get("https://www.coinspot.com.au/pubapi/v2/latest").await?;
 
-        let res_json: LatestPricesResponse = serde_json::from_str(&res.text().await?)?;
-
-        Ok(res_json)
+        match res.status() {
+            StatusCode::OK => {
+                let res_json: LatestPricesResponse = serde_json::from_str(&res.text().await?)?;
+                return Ok(
+                    CoinSpotResponse::Ok(res_json)
+                )
+            },
+            StatusCode::BAD_REQUEST => {
+                let res_json: CoinSpotBadResponse = serde_json::from_str(&res.text().await?)?;
+                return Ok(
+                    CoinSpotResponse::Bad(res_json)
+                )
+            },
+            _ => return Err(format!("Failed to consider the status {:?}", res.status()).into())
+        }
     }
 
 }
@@ -40,11 +52,16 @@ mod tests {
     #[tokio::test]
     async fn test_latest_prices() {
     
-        let result = CoinSpotPublic::latest_prices().await.unwrap();
+        let result: CoinSpotResponse<LatestPricesResponse> = CoinSpotPublic::latest_prices().await.unwrap();
 
-        assert_eq!(result.status, "ok");
+        match result {
+            CoinSpotResponse::Ok(res) => {
+                assert_eq!(res.status, "ok");
+                assert!(res.prices.contains_key("btc"));
+            },
+            _ => {}
+        }
 
-        assert!(result.prices.contains_key("btc"));
         
     }
 }

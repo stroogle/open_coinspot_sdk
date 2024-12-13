a541ccf4191c9a856fb3bab2c4b81e688df3064d
Colm Moore <colm.moore3@gmail.com>
2024-12-13 21:48:21 +1100

    ✨ Latest buy price added

---
+++ src/v2/public_api/latest_buy_price.rs
@@ -0,0 +1,83 @@
+use reqwest::StatusCode;
+use serde::{Deserialize, Serialize};
+
+use crate::v2::{CoinSpotBadResponse, CoinSpotPublic, CoinSpotResponse, CoinSpotResult};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct LatestBuyPrice {
+    pub status: String,
+    pub message: String,
+    pub rate: String,
+    pub market: String,
+}
+
+impl CoinSpotPublic {
+
+    /// Used to get the latest buy price of a specific coin.
+    /// CoinSpot's API also throws a 400 error for invalid markets.
+    /// This 400 error will return a CoinSpotResponse::Bad response
+    pub async fn latest_buy_price(coin_symbol: &str) -> CoinSpotResult<LatestBuyPrice>{
+        let url = format!("https://www.coinspot.com.au/pubapi/v2/buyprice/{}", coin_symbol);
+        println!("{:?}", &url);
+        let res = reqwest::get(
+            &url
+        ).await?;
+
+        match res.status() {
+            StatusCode::OK => {
+                let text = res.text().await?;
+                println!("{:?}", &text);
+                let json: LatestBuyPrice = serde_json::from_str(&text)?;
+                return Ok(
+                    CoinSpotResponse::Ok(json)
+                )
+            },
+            StatusCode::BAD_REQUEST => {
+                let text = res.text().await?;
+                println!("{:?}", &text);
+                let json: CoinSpotBadResponse = serde_json::from_str(&text)?;
+                return Ok(
+                    CoinSpotResponse::Bad(json)
+                )
+            }
+            _ => {
+                return Err(format!("CoinSpot API never expects status: {:?}", res.status()).into())
+            }
+        }
+    }
+
+}
+
+mod tests {
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_latest_buy_price() {
+    
+        let result = CoinSpotPublic::latest_buy_price("btc").await.unwrap();
+
+        match result {
+            CoinSpotResponse::Ok(res) => {
+                assert_eq!(res.status, "ok");
+                assert_eq!(res.message, "ok");
+            },
+            _ => {}
+        }    
+    }
+
+    #[tokio::test]
+    async fn test_latest_buy_price_fake_coin() {
+        let result2 = CoinSpotPublic::latest_buy_price("sdfsdf")
+        .await
+        .unwrap();
+
+        match result2 {
+            CoinSpotResponse::Bad(res) => {
+                assert_eq!(res.status, "error");
+                assert_eq!(res.message, "Coin not found");
+            },
+            _ => assert!(false)
+        }
+    }
+}
--- src/v2/public_api/mod.rs
+++ src/v2/public_api/mod.rs
@@ -1,3 +1,4 @@
 pub mod latest_prices;
 pub mod latest_coin_price;
 pub mod latest_coin_price_market;
+pub mod latest_buy_price;
